3dbb028d2b1ac25bcdc2c173cea7a9b9cc79f73e
Sanjaya Bhattarai <t3bhsa00@students.oamk.fi>
2025-03-20 00:26:51 +0200

    Landing page Completed

--- app/src/main/java/com/example/wheeldeal/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/wheeldeal/ui/screens/HomeScreen.kt
@@ -1,2 +1,33 @@
-package com.example.wheeldeal.ui.screens
+package com.example.wheeldeal.ui.screen
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Welcome to the Home Screen!")
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(text = "You can now explore more content here.")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
